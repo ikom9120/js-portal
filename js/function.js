7093e2e1ebacae20f9421ae6107a5372246e6b88
ikom9120 <ikom9120@gmail.com>
2020-11-27 16:25:48 +0200

    JP-4 Добавлено подтягивание новостей с backend

---
+++ js/function.js
@@ -0,0 +1,32 @@
+/**
+ * Ф-ция отправляет запрос для подтягивания новостей с backend
+ */
+function getNews() {
+    $.ajax({
+        url: '/backend/news.php',
+        type: 'get',
+        dataType: 'json',
+        timeout: 5 * 1000,
+        success: function (response) {
+            $('.load').hide();
+            for (let i = 0; i < response.length; i++) {
+                $('.news').append(response[i]['title'] + '<br>');
+                $('.news').append('<img src="' + response[i]['image'] + '" class="img-news" > <br>');
+                $('.news').append('<span class="text-news">' + response[i]['text'] + '</span> <br>');
+                $('.news').append('<div class="indent"></div>');
+                $('.news').append('<span class="created-news">' + response[i]['created_at'] + '</span>');
+                $('.news').append('<hr><br>');
+            }
+        },
+        error: function (response) {
+            Swal.fire(
+                'Ошибка отправки'
+            );
+        }
+    });
+}
+
+
+
+
+
