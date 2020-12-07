778a43246635a771126ff052f36b928c96d63a56
ikom9120 <ikom9120@gmail.com>
2020-12-07 04:14:22 +0200

    JP-6 Добавлены backend & frontend части погоды

---
+++ backend/weather.php
@@ -0,0 +1,20 @@
+<?php
+echo '{
+	"odessa": {
+		"yesterday": {
+			"degrees": 6,
+			"type": "snow",
+			"humidity": "93%"
+		},
+		"today": {
+			"degrees": 5,
+			"type": "rain",
+			"humidity": "95%"
+		},
+		"tomorrow": {
+			"degrees": 7,
+			"type": "sun",
+			"humidity": "92%"
+		}
+	}
+}';
